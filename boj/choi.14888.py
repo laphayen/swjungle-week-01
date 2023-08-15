b280f30e102379de8005d09c3bd3f787418366a6
ChoiWheatley <chltmdgus604@naver.com>
2023-08-15 19:55:46 +0900

    accept(14888): 연산자 끼워넣기 -- 연산자 순회

--- boj/choi.14888.py
+++ boj/choi.14888.py
@@ -1,72 +1,100 @@
-from enum import Enum
-from functools import total_ordering
-from itertools import permutations
-from typing import List
-
 standard_input = """6
 1 2 3 4 5 6
 2 1 1 1
 """
 
 
-@total_ordering
-class Op(Enum):
-    """참고: https://stackoverflow.com/a/39269589/21369350"""
-
-    Add = 0
-    Sub = 1
-    Prd = 2
-    Div = 3
-
-    def __lt__(self, other):
-        if self.__class__ is other.__class__:
-            return self.value < other.value
-        return NotImplemented
-
-
-MAPPER = [Op.Add, Op.Sub, Op.Prd, Op.Div]
-
-
-def calc_one(x: int, y: int, operator: Op) -> int:
-    if operator is Op.Div and y == 0:
-        raise ZeroDivisionError
-    match operator:
-        case (Op.Add):
-            return x + y
-        case (Op.Sub):
-            return x - y
-        case (Op.Prd):
-            return x * y
-        case (Op.Div):
-            if x < 0:
-                # -1 // 3 => 0이 아니라 -1이 나와서 버그
-                return -(abs(x) // y)
-            return x // y
-
-
-def calculate(num_ls: List[int], op_ls: List[Op]) -> int:
-    result = num_ls[0]
-    for idx, num in enumerate(num_ls[1:]):
-        result = calc_one(result, num, op_ls[idx])
-    return result
+def r_sol(
+    idx,
+    total,
+    addition_cnt,
+    subtraction_cnt,
+    product_cnt,
+    division_cnt,
+    on_leaf,
+    num_ls,
+):
+    """idx가 0이 아니라 1부터 시작하고 total이 ls[0]부터 시작한다는 점 참고"""
+    if idx == len(num_ls):
+        on_leaf(total)
+        return
+    if addition_cnt:
+        r_sol(
+            idx + 1,
+            total + num_ls[idx],
+            addition_cnt - 1,
+            subtraction_cnt,
+            product_cnt,
+            division_cnt,
+            on_leaf,
+            num_ls,
+        )
+    if subtraction_cnt:
+        r_sol(
+            idx + 1,
+            total - num_ls[idx],
+            addition_cnt,
+            subtraction_cnt - 1,
+            product_cnt,
+            division_cnt,
+            on_leaf,
+            num_ls,
+        )
+    if product_cnt:
+        r_sol(
+            idx + 1,
+            total * num_ls[idx],
+            addition_cnt,
+            subtraction_cnt,
+            product_cnt - 1,
+            division_cnt,
+            on_leaf,
+            num_ls,
+        )
+    if division_cnt:
+        if total < 0:
+            total = -(abs(total) // num_ls[idx])
+        else:
+            total //= num_ls[idx]
+        r_sol(
+            idx + 1,
+            total,
+            addition_cnt,
+            subtraction_cnt,
+            product_cnt,
+            division_cnt - 1,
+            on_leaf,
+            num_ls,
+        )
 
 
 n = int(input())
 num_ls = [int(x) for x in input().split()]
-op_int_ls = [int(x) for x in input().split()]
-op_ls = []
-for id, amt in enumerate(op_int_ls):
-    for _ in range(amt):
-        op_ls.append(MAPPER[id])
+addition_cnt, subtraction_cnt, product_cnt, division_cnt = [
+    int(x) for x in input().split()
+]
+
+maximum = -1000000000
+minimum = 1000000000
 
 
-sorted(op_ls)
-_max = -1000000000
-_min = 1000000000
-for ops in permutations(op_ls):
-    result = calculate(num_ls, list(ops))
-    _max = max(_max, result)
-    _min = min(_min, result)
+def on_leaf(total):
+    global maximum
+    global minimum
+    maximum = max(maximum, total)
+    minimum = min(minimum, total)
 
-print(_max)
-print(_min)
+
+r_sol(
+    1,
+    num_ls[0],
+    addition_cnt,
+    subtraction_cnt,
+    product_cnt,
+    division_cnt,
+    on_leaf,
+    num_ls,
+)
+
+print(maximum)
+print(minimum)
